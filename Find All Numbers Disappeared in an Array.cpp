6f37ccaaefa2b268dcd18ec5996e89b0901e57e9
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-05-10 20:30:01 +0530

    448. Find All Numbers Disappeared in an Array
    https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/

---
+++ Find All Numbers Disappeared in an Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        
+        int n=nums.size();
+        vector<int > hash(n+1);
+        
+        for(int i=0;i<n;i++)
+        {
+            hash[nums[i]]++;
+        }
+        vector<int> missing;
+        for(int i=1;i<=n;i++)
+        {
+            if(hash[i]==0)
+                missing.push_back(i);
+        }
+        return missing;
+        
+    }
+};
