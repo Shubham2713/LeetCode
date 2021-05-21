f2e7209f9eff6a4a14bef087a21a9c446b753849
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-05-21 12:51:12 +0530

    1. Two Sum
    https://leetcode.com/problems/two-sum/

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -1,30 +1,3 @@
-/* O(nxn) time O(1)
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector <int> ans;
-        int c=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            for(int j=i+1;j<nums.size();j++)
-            {
-                if(nums[j]==(target-nums[i]))
-                {
-                  ans.push_back(i);
-                    ans.push_back(j);
-                    c=1;
-                    break;
-                } 
-                
-            }
-            if(c==1)
-                break;
-        }
-        return ans;
-        
-    }
-};
-*/
 //O(n) time O(n) space
 class Solution {
 public:
@@ -52,3 +25,33 @@ public:
         return res;
     }
 };
+
+
+/* O(nxn) time O(1)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector <int> ans;
+        int c=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[j]==(target-nums[i]))
+                {
+                  ans.push_back(i);
+                    ans.push_back(j);
+                    c=1;
+                    break;
+                } 
+                
+            }
+            if(c==1)
+                break;
+        }
+        return ans;
+        
+    }
+};
+*/
+
