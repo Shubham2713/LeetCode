b1ca00f21ec62afd66f1863793bedb05d342ef87
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-05-24 18:51:59 +0530

    22. Generate Parentheses
    https://leetcode.com/problems/generate-parentheses/

---
+++ Generate Parentheses.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool isBalanced(string str)
+    {
+        stack <char> s;
+        int n=str.length();
+        if(str[0]=='(')
+            s.push('(');
+        else return false;
+        for(int i=1;i<n;i++)
+        {
+            if(str[i]=='(')
+                s.push(str[i]);
+           else if(str[i]==')'&&s.empty()==false&&s.top()=='(')
+                s.pop();
+            else
+                return false;
+        }
+        if(s.empty()==false)
+            return false;
+        else
+            return true;
+        
+    }
+    
+    
+    vector<string> generateParenthesis(int n) {
+        string str="";
+        for(int i=0;i<n;i++)
+        {str+="(";}
+        for(int i=0;i<n;i++)
+        {str+=")";}
+        vector <string > ans;
+        
+        do
+        {
+         if(isBalanced(str)==true)
+            //cout<<str<<"\n";
+             ans.push_back(str);
+        }while(next_permutation(str.begin(),str.end()));
+            
+        return ans;
+        
+    }
+};
