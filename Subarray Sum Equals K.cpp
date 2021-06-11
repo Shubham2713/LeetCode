fa25ebb2267192f45f8f6b75f09e9c4e3b1311dc
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-06-11 11:14:44 +0530

    Subarray Sum Equals K
    https://leetcode.com/problems/subarray-sum-equals-k/

---
+++ Subarray Sum Equals K.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        int res=0;
+        unordered_map<int,int > s;
+        s[0]=1;
+        int ps=0;
+        for(int i=0;i<n;i++)
+        {
+            ps+=nums[i];
+            if(s.find(ps-k)!=s.end())
+            {
+                res+=s[ps-k];
+                
+            }
+        
+                s[ps]++;
+        }
+        return res;
+    }
+};
