4a0fde2205ae0231f1cd10c6ce08103c826866a6
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-07-15 21:46:22 +0530

    162. Find Peak Element
    https://leetcode.com/problems/find-peak-element/

--- Find Peak Element.cpp
+++ Find Peak Element.cpp
@@ -1,3 +1,30 @@
+//binary search
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        int lo=0,hi=n-1,mid;
+        
+        while(lo<hi )
+        {
+            mid=lo+(hi-lo)/2;
+            
+            if(mid+1<n && mid-1>=0 &&nums[mid]>nums[mid+1]&&nums[mid]>nums[mid-1])
+                return mid;
+            else if(mid+1<n&&nums[mid]<nums[mid+1])
+                lo=mid+1;
+            else if(mid-1>=0)
+                hi=mid-1;
+            else
+                break;
+        }
+        return lo;
+        
+        
+    }
+};
+
+
 //Brute force technique
 
 class Solution {
