56b66986eccc5b7e42d86ed57367828098bf9559
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-07-19 21:51:40 +0530

    41. First Missing Positive
    https://leetcode.com/problems/first-missing-positive/

--- First Missing Positive.cpp
+++ First Missing Positive.cpp
@@ -1,3 +1,39 @@
+//optimized
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n=nums.size();
+        int k=0,l=n-1;
+        while(k<=l)
+        {
+            if(nums[k]>0)
+            {
+                k++;
+                continue;
+                
+            }
+            swap(nums[k],nums[l]);
+            l--;
+        }
+        int new_size=l+1;
+        
+        for(int i=0;i<new_size;i++)
+        {
+            if(abs(nums[i])>new_size)
+                continue;
+            if(nums[abs(nums[i])-1]>0)
+                nums[abs(nums[i])-1]*=-1;
+        }
+        for(int i=0;i<new_size;i++)
+            if(nums[i]>0)
+                return i+1;
+        
+        return new_size+1;
+        
+    }
+};
+
+
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
