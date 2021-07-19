9de8e583fbc30d93a0b67f210ea8933f532d8c5d
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-07-19 21:02:42 +0530

    287. Find the Duplicate Number
    https://leetcode.com/problems/find-the-duplicate-number/

--- Find the Duplicate Number.cpp
+++ Find the Duplicate Number.cpp
@@ -1,3 +1,20 @@
+//optimized
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(nums[abs(nums[i])]<0)
+                return abs(nums[i]);
+            nums[abs(nums[i])]*=-1;
+        }
+        return -1;
+    }
+};
+
+
 //time O(n) space O(1)
 class Solution {
 public:
