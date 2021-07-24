98e7a277bf8d54d1a12f780fc3031b7741d50315
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-07-24 09:47:23 +0530

    1267. Count Servers that Communicate
    https://leetcode.com/problems/count-servers-that-communicate/

--- Count Servers that Communicate.cpp
+++ Count Servers that Communicate.cpp
@@ -1,3 +1,38 @@
+//optimized
+class Solution {
+public:
+    int countServers(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<int> row(m,0);
+        vector<int> col(n,0);
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j])
+                {
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        int c=0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]&&(col[j]>1||row[i]>1))
+                    c++;
+            }
+        }
+        return c;
+        
+    }
+};
+
+
 class Solution {
 public:
     int countServers(vector<vector<int>>& grid) {
