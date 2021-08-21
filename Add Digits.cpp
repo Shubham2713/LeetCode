157a962a434e3b87783bea42e9317ce15359cc24
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-08-21 21:46:22 +0530

    258. Add Digits
    https://leetcode.com/problems/add-digits/

--- Add Digits.cpp
+++ Add Digits.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int addDigits(int num) {
+        int ans=0;
+        while(num)
+        {
+            ans+=num%10;
+            num/=10;
+        }
+        if(ans/10==0)
+            return ans;
+        else
+            return addDigits(ans);
+        
+    }
+};
+
+class Solution {
+public:
+    int addDigits(int num) {
         while(num>=10)
         {
             int c=num;
