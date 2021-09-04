86ee5e851204314a2fa38465d4603e6bcda8e7f2
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-09-05 00:41:00 +0530

    118. Pascal's Triangle
    https://leetcode.com/problems/pascals-triangle/

---
+++ Pascal's Triangle.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        
+        vector<vector<int>> res;
+        if(numRows==1)
+           return {{1}};
+        if(numRows==2)
+            return {{1},{1,1}};
+        
+        res.push_back({1});
+        res.push_back({1,1});
+        
+        for(int i=2;i<numRows;i++)
+        {
+            vector<int> temp(i+1,1);
+            res.push_back(temp);
+            for(int j=1;j<=i-1;j++)
+                res[i][j]=res[i-1][j-1]+res[i-1][j];
+            
+        }
+        
+       return res; 
+    }
+};
