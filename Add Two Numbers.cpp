671c1f99ab262a737f139c1fb5a53e5388a150f8
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2022-03-06 12:09:39 +0530

    2. Add Two Numbers
    https://leetcode.com/problems/add-two-numbers/

--- Add Two Numbers.cpp
+++ Add Two Numbers.cpp
@@ -10,6 +10,70 @@
  */
 class Solution {
 public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry=0;
+        ListNode* res=new ListNode((l1->val+l2->val)%10);
+        ListNode * ret=res;
+        carry=(l1->val+l2->val)/10;
+        
+        l1=l1->next;
+        l2=l2->next;
+        
+        while(l1 && l2)
+        {
+            int sum=l1->val+l2->val+carry;
+            
+            ListNode* temp=new ListNode(sum%10);
+            carry=sum/10;
+            res->next=temp;
+            res=res->next;
+            l1=l1->next;
+            l2=l2->next;
+        }
+        
+        while(l1)
+        {
+            int sum=l1->val+carry;
+            ListNode* temp=new ListNode(sum%10);
+            carry=sum/10;
+            res->next=temp;
+            res=res->next;
+            l1=l1->next;
+        }
+        
+        while(l2)
+        {
+            int sum=l2->val+carry;
+            ListNode* temp=new ListNode(sum%10);
+            carry=sum/10;
+            res->next=temp;
+            res=res->next;
+            l2=l2->next;
+        }
+        
+        if(carry>0)
+        {
+            ListNode* temp= new ListNode(carry);
+            res->next=temp;
+        
+        }
+        
+        return ret;
+        
+    }
+};
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
     ListNode *last=nullptr;
   void add(int v)
     {
